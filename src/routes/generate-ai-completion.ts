ccde93493f020ecaed4eb21541b862d897071fb7
Douglas Camacho <douglas.lopescamacho@gmail.com>
2023-09-16 10:00:26 -0300

    back-end quase pronto

---
+++ src/routes/generate-ai-completion.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify";
+import { createReadStream } from 'node:fs'
+import { z } from 'zod';
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+
+export async function generateAICompletionRoute(app: FastifyInstance) {
+    app.post('/ai/complete', async (req) => {
+        const bodySchema = z.object({
+            prompt: z.string(),
+        })
+
+        const { prompt } = bodySchema.parse(req.body)
+
+    })
+}
